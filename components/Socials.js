eb9fd0f67a833493cf179df1971cf6bc84794c3e
alejandrouran <alejandrourn@gmail.com>
2023-11-27 14:15:13 -0500

    update portfolio starter

--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,20 @@
+import Link from "next/link";
+import { RiLinkedinFill, RiGithubFill, RiTwitterFill } from 'react-icons/ri'
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+  <div className="flex items-center gap-x-5 text-lg">
+    <Link href={''} className="hover:text-accent transition-all duration-300">
+      <RiLinkedinFill />
+    </Link> 
+    <Link href={''} className="hover:text-accent transition-all duration-300">
+      <RiGithubFill />
+    </Link> 
+    <Link href={''} className="hover:text-accent transition-all duration-300">
+      <RiTwitterFill />
+    </Link> 
+  </div>
+  );
 };
 
 export default Socials;
